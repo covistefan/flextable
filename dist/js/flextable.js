f770754f0796d2475d6751b8099d0e08f755d7b7
Stefan Händler <covistefan@users.noreply.github.com>
2018-04-10 10:03:53 +0200

    Version 1.1.0

--- dist/js/flextable.js
+++ dist/js/flextable.js
@@ -1,13 +1,13 @@
 /*
- * jQuery FlexTable v1.0.0
- * https://www.github.com/covistefan/flextable
+ * jQuery FlexTable v1.1.0
+ * https://www.covi.de/jquery/flextable
  *
  * Copyright 2018 COVI.DE
  * Date 2018-04-09
  * Free to use under the GPLv2 and later license.
  * http://www.gnu.org/licenses/gpl-2.0.html
  *
- * Contributing author: Stefan Händler (@covistefan)
+ * Contributing author: Stefan Händler (@appleuser)
  * 
  */
 
@@ -49,20 +49,20 @@ function flexTable(tableData, dataOptions) {
         // if no heading cells were found
         if (headCount==0) {
             // search for defined heading cells
-            if (dataOptions.pcell!=''){
-                tableData.find(dataOptions.pcell).parent().attr('flextable', 'header');
-                tableData.find(dataOptions.pcell).each (function() {
+            if (dataOptions.hcell!=''){
+                tableData.find(dataOptions.hcell).parent().attr('flextable', 'header');
+                tableData.find(dataOptions.hcell).each (function() {
                     headData.push(this.innerHTML);
                     // adding attr to holding element to prevent later data usage
                 });
             }
             // search for element holding heading cells
-            else if (dataOptions.phead!='') {
-                tableData.find(dataOptions.phead).children().each (function() {
+            else if (dataOptions.hparent!='') {
+                tableData.find(dataOptions.hparent).children().each (function() {
                     // adding header data to array
                     headData.push(this.innerHTML);
                     // adding attr to holding element to prevent later data usage
-                    tableData.find(dataOptions.phead).attr('flextable', 'header');
+                    tableData.find(dataOptions.hparent).attr('flextable', 'header');
                 });
             }
         }
@@ -83,31 +83,32 @@ function flexTable(tableData, dataOptions) {
         });
         
         // building final list
-        var ul = $("<ul>");
+        var ul = $("<ul>"); 
         ul.attr('class', tableData.attr('class'));
         ul.addClass('flextable-list');
         ul.attr('id', 'flextable-'+tableData.attr('id'));
         for (var r = 0; r<cellData.length; r++) {
             for (var c = 0; c<cellData[r].length; c++) {
                 if (dataOptions.header=='after') {
-                    ul.append("<li class='flextable-data'>" + cellData[r][c] + "</li><li class='flextable-head'>" + headData[c] + "</li>");
+                    var li = "<li class='flextable-data'>" + cellData[r][c] + "</li><li class='flextable-head'>" + headData[c] + "</li>";
+                    if (dataOptions.combine) { li = "<li class='flextable-row'><ul>" + li + "</ul></li>"; }
+                    ul.append(li);
                 }
                 else if (dataOptions.header=='none') {
                     ul.append("<li class='flextable-data'>" + cellData[r][c] + "</li>");
                 }
                 else {
-                    ul.append("<li class='flextable-head'>" + headData[c] + "</li><li class='flextable-data'>" + cellData[r][c] + "</li>");
+                    var li = "<li class='flextable-head'>" + headData[c] + "</li><li class='flextable-data'>" + cellData[r][c] + "</li>";
+                    if (dataOptions.combine) { li = "<li class='flextable-row'><ul>" + li + "</ul></li>"; }
+                    ul.append(li);
                 }
             }
             
             
             
         }
-        
+        // inserting ul after table in DOM
         tableData.after(ul);
-        
-        console.log(ul);
-        
     }	
 }
 
@@ -127,16 +128,16 @@ function flexTable(tableData, dataOptions) {
             flexTable($this, opts);
         });
     });
-    
 }
     
 $.fn.flextable.defaults = {
-	debug: true, // debug mode
+	debug: false, // debug mode
 	bt: 'parent', // BreakType 'parent' » parent element width, 'window' » based on window width
 	bw: 768, // BreakWidth int » only with bt: 'window', if window width is lower then bw
-	cm: 'auto', // CalcMode 'auto' » onload and onresize, 'load' » only on load, 'resize' » only on resize
-    phead: '', // selector of parent holding header cells to get tablehead if not defined by thead » used if thead not found AND pcell is empty 
-    pcell: '', // selector of cells to get tablehead if not defined by thead » used if thead not found 
+	cm: 'auto', // CalcMode 'auto' » on load and on resize, 'load' » only on load
+    hparent: '', // selector of parent holding header cells to get tablehead if not defined by thead » used if thead not found AND hcell is empty 
+    hcell: '', // selector of cells to get tablehead if not defined by thead » used if thead not found 
     header: 'before', // showing tablehead information 'before' » add li with header data before data cell, 'after' » add li with header data after data cell, 'none' » ignore header data
+    combine: false // display data as simple list, true » create a '<li><ul><li header></li><li data></li></ul></li>' set if header is included
     };
 })(jQuery)
